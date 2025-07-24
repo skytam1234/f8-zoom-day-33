94bdb927a2dbcabeab7470207731dd42a5f176e7
Anh Duc <skytam1234@gmail.com>
2025-07-24 16:38:52 +0700

    tạm ổn

--- components/app-modal.js
+++ components/app-modal.js
@@ -1,91 +1,98 @@
 import * as DAO from "../httpRequest/httpRequest.js";
 class AppModal extends HTMLElement {
-  isInsert = true;
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
-  connectedCallback() {
-    const template = document.querySelector(`#app-modal-tpl`);
-    const appModalNode = template.content.cloneNode(true);
-    this.shadowRoot.appendChild(appModalNode);
-    this.shadowRoot.addEventListener("click", (e) => {
-      const modalContainer = this.shadowRoot.querySelector(`#addTaskModal`);
-      if (!modalContainer.classList.contains("show")) {
-        return;
-      }
-      e.stopPropagation();
-      const cancelBtn = e.target.closest(`.btn-cancel`);
-      const modal = this.shadowRoot.querySelector(`.modal`);
-      const isOverlay = modal && !modal.contains(e.target);
-      const form = this.shadowRoot.querySelector(`.todo-app-form`);
-      const modalClose = e.target.closest(`.modal-close`);
-      if (cancelBtn || modalClose || isOverlay) {
-        if (confirm("Bạn chắc chắn muốn tắt  form?")) {
-          this.close();
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.data = null;
+        this.oldTask = null;
+    }
+    connectedCallback() {
+        const template = document.querySelector(`#app-modal-tpl`);
+        const appModalNode = template.content.cloneNode(true);
+        this.shadowRoot.appendChild(appModalNode);
+        this.modal = this.shadowRoot.querySelector(`.modal`);
+        this.modalApp = this.shadowRoot.querySelector(`#addTaskModal`);
+        this.form = this.shadowRoot.querySelector(`.todo-app-form`);
+        this.taskTitle = this.shadowRoot.querySelector(`#taskTitle`);
+        this.taskDescription =
+            this.shadowRoot.querySelector(`#taskDescription`);
+        this.taskCategory = this.shadowRoot.querySelector(`#taskCategory`);
+        this.taskPriority = this.shadowRoot.querySelector(`#taskPriority`);
+        this.startTime = this.shadowRoot.querySelector(`#startTime`);
+        this.endTime = this.shadowRoot.querySelector(`#endTime`);
+        this.taskDate = this.shadowRoot.querySelector(`#taskDate`);
+        this.taskColor = this.shadowRoot.querySelector(`#taskColor`);
+        this.shadowRoot.addEventListener("click", (e) => {
+            const modalContainer =
+                this.shadowRoot.querySelector(`#addTaskModal`);
+            if (!modalContainer.classList.contains("show")) {
+                return;
+            }
+            e.stopPropagation();
+            const cancelBtn = e.target.closest(`.btn-cancel`);
+            const modal = this.shadowRoot.querySelector(`.modal`);
+            const isOverlay = modal && !modal.contains(e.target);
+            const form = this.shadowRoot.querySelector(`.todo-app-form`);
+            const modalClose = e.target.closest(`.modal-close`);
+            const confirmBtn = e.target.closest(`.btn-submit`);
+            if (cancelBtn || modalClose || isOverlay) {
+                if (confirm("Bạn chắc chắn muốn tắt  form?")) {
+                    this.data = null;
+                    this.close();
+                }
+            }
+            if (confirmBtn) {
+                this.close();
+            }
+        });
+        document.addEventListener("keydown", (e) => {
+            if (e.key === `Escape`) {
+                if (confirm("Bạn chắc chắn muốn tắt  form?")) {
+                    this.close();
+                }
+            }
+        });
+    }
+
+    open() {
+        this.form.reset();
+        this.modal.scrollTop = 0;
+        this.modalApp.classList.add(`show`);
+        setTimeout(() => {
+            this.taskTitle.focus();
+        }, 100);
+        this.data = `add`;
+        this.dispatchEvent(new CustomEvent("open"));
+    }
+    edit(oldTask) {
+        this.oldTask = oldTask;
+        this.modalApp.classList.add(`show`);
+        this.data = "edit";
+        this.taskTitle.value = this.oldTask.title;
+        this.taskDescription.value = this.oldTask.description;
+        this.taskCategory.value = this.oldTask.category;
+        this.taskPriority.value = this.oldTask.priority;
+        this.startTime.value = this.oldTask.startTime;
+        this.endTime.value = this.oldTask.endTime;
+        this.taskDate.value = this.oldTask.dueDate;
+        this.taskColor.value = this.oldTask.color;
+    }
+    async close() {
+        if (this.data === "add") {
+            const newTask = await Object.fromEntries(new FormData(this.form));
+            console.log(newTask);
+            await DAO.createTask(newTask);
         }
-      }
-    });
-    this.shadowRoot.addEventListener("keydown", (e) => {
-      if (e.key === `Escape`) {
-        if (confirm("Bạn chắc chắn muốn tắt  form?")) {
-          this.close();
+        if (this.data === "edit") {
+            const newTask = await Object.fromEntries(new FormData(this.form));
+            const task = await DAO.editTask(this.oldTask.id, newTask);
         }
-      }
-    });
-  }
-  open() {
-    this.isInsert = true;
-    const modal = this.shadowRoot.querySelector(`.modal`);
-    const taskTitle = this.shadowRoot.querySelector(`#taskTitle`);
-    const modalApp = this.shadowRoot.querySelector(`#addTaskModal`);
-    const form = this.shadowRoot.querySelector(`.todo-app-form`);
-    form.reset();
-    modal.scrollTop = 0;
-    modalApp.classList.add(`show`);
-    setTimeout(() => {
-      taskTitle.focus();
-    }, 100);
-    const confirmBtn = this.shadowRoot.querySelector(`.btn-submit`);
-    confirmBtn.onclick = () => {
-      const newTask = Object.fromEntries(new FormData(form));
-      DAO.createTask(newTask);
-      this.close();
-    };
-    this.dispatchEvent(new CustomEvent("open"));
-  }
-  edit(oldTask) {
-    this.isInsert = false;
-    const modal = this.shadowRoot.querySelector(`.modal`);
-    const modalApp = this.shadowRoot.querySelector(`#addTaskModal`);
-    const form = this.shadowRoot.querySelector(`.todo-app-form`);
-    const taskTitle = this.shadowRoot.querySelector(`#taskTitle`);
-    const taskDescription = this.shadowRoot.querySelector(`#taskDescription`);
-    const taskCategory = this.shadowRoot.querySelector(`#taskCategory`);
-    const taskPriority = this.shadowRoot.querySelector(`#taskPriority`);
-    const startTime = this.shadowRoot.querySelector(`#startTime`);
-    const endTime = this.shadowRoot.querySelector(`#endTime`);
-    const taskDate = this.shadowRoot.querySelector(`#taskDate`);
-    const taskColor = this.shadowRoot.querySelector(`#taskColor`);
-    taskTitle.value = oldTask.title;
-    taskDescription.value = oldTask.description;
-    taskCategory.value = oldTask.category;
-    taskPriority.value = oldTask.priority;
-    startTime.value = oldTask.startTime;
-    endTime.value = oldTask.endTime;
-    taskDate.value = oldTask.dueDate;
-    taskColor.value = oldTask.color;
-    modalApp.classList.add(`show`);
-    const confirmBtn = this.shadowRoot.querySelector(`.btn-submit`);
-    confirmBtn.onclick = () => {
-      const newTask = Object.fromEntries(new FormData(form));
-      DAO.editTask(oldTask.id, newTask);
-      this.close();
-    };
-  }
-  close() {
-    this.shadowRoot.querySelector(`#addTaskModal`).classList.remove(`show`);
-    this.dispatchEvent(new CustomEvent("close"));
-  }
+        await this.dispatchEvent(
+            new CustomEvent("close", { detail: this.data })
+        );
+        await this.shadowRoot
+            .querySelector(`#addTaskModal`)
+            .classList.remove(`show`);
+    }
 }
 customElements.define(`app-modal`, AppModal);
